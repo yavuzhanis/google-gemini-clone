c14c118e6cb52f8713512fe634bed659152b38d5
Yavuzhan <yavuzhan.is@kun.edu.tr>
2024-03-11 15:37:10 +0300

    gemini-clone-react
    reactJS project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Main from "./components/main/Main"
+import Sidebar from "./components/sideBar/Sidebar"
+
+function App() {
+  return (
+    <>
+      <Sidebar />
+      <Main />
+    </>
+  )
+}
+
+export default App
